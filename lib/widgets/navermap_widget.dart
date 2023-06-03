fa26292cf1d39684347007f938247f899bbe51bc
ferrorist <ferrorist@naver.com>
2023-06-04 00:02:48 +0900

    navermap

--- lib/widgets/navermap_widget.dart
+++ lib/widgets/navermap_widget.dart
@@ -1,56 +1,45 @@
-﻿import 'package:flutter/material.dart';
-// import 'package:flutter_naver_map/flutter_naver_map.dart';
+﻿import 'dart:developer';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:geolocator/geolocator.dart';
 
-class MyNaverMap extends StatelessWidget {
+import '../services/gps_service.dart';
+
+class MyNaverMap extends StatefulWidget {
   const MyNaverMap({
     super.key,
-    required this.userPosition,
+    required this.ViewType,
   });
 
-  final Future<Position> userPosition;
+  final String ViewType;
+
+  @override
+  State<MyNaverMap> createState() => _MyNaverMapState();
+}
+
+class _MyNaverMapState extends State<MyNaverMap> {
+  static const platform = MethodChannel("UserTmap");
+  late Position user_position;
+  late double latitude, longitude;
+
+  Future<void> sendUserLatLng() async {
+    try {
+      await platform.invokeMethod(
+          'sendUserLatLng', {'latitude': latitude, 'longitude': longitude});
+    } on PlatformException catch (e) {
+      log('Error: $e');
+    }
+  }
+
+  Future<void> getUserPosition() async {
+    user_position = await GPSService.getPosition();
+    latitude = user_position.latitude;
+    longitude = user_position.longitude;
+    sendUserLatLng();
+  }
 
   @override
   Widget build(BuildContext context) {
     return const Text("Not Widget"); // return FutureBuilder(
-    //   future: userPosition,
-    //   builder: (context, snapshot) {
-    //     if (snapshot.hasData) {
-    //       return NaverMap(
-    //         options: NaverMapViewOptions(
-    //           initialCameraPosition: NCameraPosition(
-    //             target: NLatLng(
-    //               snapshot.data!.latitude,
-    //               snapshot.data!.longitude,
-    //             ),
-    //             zoom: 16,
-    //           ),
-    //           // 실내 지도 활성화 유무
-    //           indoorEnable: true,
-    //           // 표시할 정보 레이어
-    //           activeLayerGroups: [
-    //             // 건물 레이어
-    //             NLayerGroup.building,
-    //           ],
-    //         ),
-    //         onMapReady: (controller) {
-    //           NMarker userMarker = NMarker(
-    //             id: "User",
-    //             position: NLatLng(
-    //               snapshot.data!.latitude,
-    //               snapshot.data!.longitude,
-    //             ),
-    //             size: const Size(20, 30),
-    //           );
-    //           controller.addOverlay(userMarker);
-    //         },
-    //       );
-    //     } else {
-    //       return const Center(
-    //         child: Text("Find User Position..."),
-    //       );
-    //     }
-    //   },
-    // );
   }
 }
