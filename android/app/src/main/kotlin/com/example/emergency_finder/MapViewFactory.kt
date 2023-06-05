298f0a93bb64ad9aa3209c461f4197f6325bf4f8
ferrorist <ferrorist@naver.com>
2023-06-06 04:49:56 +0900

    네이버 지도 widget 생성

--- android/app/src/main/kotlin/com/example/emergency_finder/MapViewFactory.kt
+++ android/app/src/main/kotlin/com/example/emergency_finder/MapViewFactory.kt
@@ -1,14 +1,13 @@
 package com.example.emergency_finder
 
 import android.content.Context
-import androidx.fragment.app.FragmentActivity
 import io.flutter.plugin.common.StandardMessageCodec
 import io.flutter.plugin.platform.PlatformView
 import io.flutter.plugin.platform.PlatformViewFactory
 
-class UserMapViewFactory(private val activity: FragmentActivity) : PlatformViewFactory(StandardMessageCodec.INSTANCE) {
+class MapViewFactory() : PlatformViewFactory(StandardMessageCodec.INSTANCE) {
     override fun create(context: Context?, viewId: Int, args: Any?): PlatformView {
         val creationParams = args as Map<String?, Any?>?
-        return MyNaverMapView(activity, context, viewId, args)
+        return NaverMapView(context, viewId, args)
     }
 }
